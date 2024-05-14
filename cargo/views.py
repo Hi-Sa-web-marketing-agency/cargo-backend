b39176e47a33298b91bdbf4f3f954078ec9ea8e3
Hi-Sa-web-marketing-agency <hisa.webmarketingagency@gmail.com>
2024-05-14 14:24:26 +0530

    post updation33

--- cargo/views.py
+++ cargo/views.py
@@ -47,7 +47,7 @@ def Enquiry_post(request):
         # Extract data from the query parameters (GET request)
         data = request.POST  # This extracts data from the query parameters
 
-        print(data'---------------------this is data---------------------------')
+        print(data,'---------------------this is data---------------------------')
 
         # Check if required fields are present
         required_fields = ['name', 'place']
